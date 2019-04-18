0736a637275e065188e46892e2fd6bc7cebbb025
jorgee1 <jorgepacheco@uninorte.edu.co>
2019-04-18 11:32:06 -0500

    Borrado de ambiente virtual

--- .Python/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/kenshinn/Documents/Proyects/Python/Django_F-R_Test/.Python/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
